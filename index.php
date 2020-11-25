9513da490733f57ed1b2853208306f1f1ccc1746
rodriguoh <jesone81@gmail.com>
2020-11-25 18:58:28 +0100

    code fini

--- index.php
+++ index.php
@@ -1,48 +1,108 @@
 <?php
-// Boilerplate for exercice DAY 3 at university MIW 05
-// Code is bad and poor... but just enough for skills students :-) 
+    // Boilerplate for exercice DAY 3 at university MIW 05
+    // Code is bad and poor... but just enough for skills students :-)
 
-// Redirect to script to send email
-if (!empty($_POST['email'])) {
+    // Redirect to script to send email
+    if (!empty($_POST['email'])) {
 
-    // Sending invitation by email
-    header('Location: send_email.php');
-    exit;
+        // Sending invitation by email
+        header('Location: send_email.php?email='.$_POST['email']);
+        exit;
 
-}
+    }
 
-$body_class = "";
+    $form_class = "";
+    $body_class = "";
 
-// Display delivery status, (tips anti-refreshing) 
-if (!empty($_GET['delivery']) and $_GET['delivery'] === "sent") {
+    // Display delivery status, (tips anti-refreshing)
+    if (!empty($_GET['delivery']) and $_GET['delivery'] === "sent") {
 
-    // Sending invitation by email
-    // echo "OK c'est envoyé..";
-    $body_class = "delivery_sent";
-}
+        // Sending invitation by email
+        // echo "OK c'est envoyé..";
+        $form_class = "form_remove";
+        $body_class = "delivery_sent";
 
+    }
+?>
 
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="Description" content="Inscris toi a la soirée de Noel de l'AMU">
+        <title>Inscription soirée étudiante</title>
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <style type="text/css">
 
-<head>
+            .form_remove{
+                display: none;
+            }
+            .delivery_sent{
+                display: block;
+                width: 500px;
+                height: 300px;
+                background: url("img/sent-mail.svg") no-repeat, rgba(255,255,255,0.5);
+                background-position-x: 60%;
+                background-position-y: 40%;
+                border-radius: 20px;
+            }
+            html{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                background: url("img/background-mobile.png") no-repeat center;
+                background-size: cover;
+            }
+            form{
+                display: flex;
+                flex-direction: column;
+            }
+            .form_elem{
+                font-size: 20px;
+                padding: 10px;
+                margin: 5px;
+                font-weight: bold;
+                text-align: left;
+                border-radius: 5px;
+            }
+            form > #email{
+                height: 45px;
+            }
+            form > #send{
+                background: url("img/email-icone.svg") no-repeat right,#C30078;
+                background-size: 40px 40px;
+                background-position-x: 85%;
+                color: white;
+                height: 70px;
+                padding-left: 20px;
+                border: none;
+            }
+            @media screen and (min-width: 769px){
+                html{
+                    background-image: url("img/background-desktop.png");
+                    background-position: top;
+                }
+                form{
+                    flex-direction: row;
+                }
+                form > #email{
+                    width: 400px;
+                }
+                form > #send{
+                    width: 250px;
+                    border-radius: 0 5px 5px 0;
+                }
+            }
+        </style>
+    </head>
 
-    <title></title>
+    <body class="<?= $body_class ?>">
+        <form action="#" method="post" class="<?php echo $form_class ?>">
+            <input type="email" name="email" id="email" placeholder="Ton email de star..." class="form_elem"  pattern="^(([^\.]+)|([^\.]+\.[^\.]+))@[^\.]+\.[a-zA-Z0-9]{2,4}$" required/>
+            <input type="submit" id="send" value="Inscris-toi !" class="form_elem"/>
+        </form>
+    </body>
 
-    <style type="text/css">
-        .delivery_sent {
-            background-color: green;
-        }
-    </style>
-
-</head>
-<body class="<?= $body_class ?>">
-
-<form action="#" method="post">
-    <input type="email" name="email" placeholder="Ton email de star..." required/>
-    <input type="submit" value="Submit"/>
-</form>
-
-</body>
 </html>
--- send_email.php
+++ send_email.php
@@ -14,32 +14,33 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    //$mail->isSMTP();                                            // Send using SMTP
-    $mail->Host = 'mail.ovh.net';                    // Set the SMTP server to send through
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host = '';                    // Set the SMTP server to send through
     $mail->SMTPAuth = true;                                   // Enable SMTP authentication
-    $mail->Username = 'VOTRE_LOGIN';                     // SMTP username
-    $mail->Password = 'PASSWORD';                               // SMTP password
-    //  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    //  $mail->Port       = 587;                                    // TCP port to connect to
+    $mail->Username = '';                     // SMTP username
+    $mail->Password = '';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;
+    $mail -> CharSet = "UTF-8";
+    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('newsletter@miw.ovh', 'MIW Party');
-    $mail->addAddress('your-name@mix.org', 'Leon');     // Add a recipient
-    $mail->addCC('newsletter@miw.ovh');
+    $mail->setFrom('contact@rodriguoh.fr', 'MIW Party');
+    $mail->addAddress($_GET['email']);     // Add a recipient
 
     // Attachments
-//    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
+    $mail->Subject = 'Inscription a la soirée MIW';
     $mail->Body = file_get_contents('email/email_party.html');
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
+    $mail->AltBody = 'Inscription a la soirée MIW';
+    header('Location: index.php?delivery=sent');
+    
     $mail->send();
     //echo 'Message has been sent';
-    header('Location: index.php?delivery=sent');
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
